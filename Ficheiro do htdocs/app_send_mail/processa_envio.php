b3dd659442b8abd8e3a95f6fec90424d9928dd08
anisio.munda <anisio.munda@jupiter.co.ao>
2022-06-16 15:51:36 +0100

    App SendMail com PHPMAILER

---
+++ Ficheiro do htdocs/app_send_mail/processa_envio.php
@@ -0,0 +1,9 @@
+<?php
+
+	require "../../../app/app_send_mail/bibliotecas/PHPMailer/Exception.php";
+	require "../../../app/app_send_mail/bibliotecas/PHPMailer/OAuth.php";
+	require "../../../app/app_send_mail/bibliotecas/PHPMailer/PHPMailer.php";
+	require "../../../app/app_send_mail/bibliotecas/PHPMailer/POP3.php";
+	require "../../../app/app_send_mail/bibliotecas/PHPMailer/SMTP.php";
+
+	require "../../../app/app_send_mail/processa_envio.php";
